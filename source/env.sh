af82eabff99912155bafcbdfc5b75692e5ca7932
s4ndhyac <sandhya2392@gmail.com>
2019-10-20 04:27:28 -0700

    env variables

---
+++ source/env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export PROJECT=capstone-project-team2
+export CLUSTER=capstone-team2-cluster
+export ZONE=us-west1-b
+export GCR_REGISTRY=gcr.io
+export FRONTEND_DOCKER_IMG=frontend
+export MEMBER_SVC_BACKEND_DOCKER_IMG=member-backend
+export ITINERARY_SVC_BACKEND_DOCKER_IMG=itinerary-backend
+export MEMBER_SVC_DB_DOCKER_IMG=member-database
+export ITINERARY_SVC_DB_DOCKER_IMG=itinerary-database
