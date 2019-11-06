4dcc52e5cbe717167ae95b3905f81951e47268a8
s4ndhyac <sandhya2392@gmail.com>
2019-11-06 01:01:43 -0800

    adding toggleSidebar reducer

--- source/frontend/src/actions/types.js
+++ source/frontend/src/actions/types.js
@@ -3,3 +3,6 @@ export const CLEAR_USER = "CLEAR_USER";
 
 export const OPEN_GROUP = "OPEN_GROUP";
 export const OPEN_ITINERARY = "OPEN_ITINERARY";
+
+export const COLLAPSE_SIDEBAR = "COLLAPSE_SIDEBAR";
+export const EXPAND_SIDEBAR = "EXPAND_SIDEBAR";
