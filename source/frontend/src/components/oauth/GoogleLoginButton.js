80ef9255ea8aba455ec50203d6ed6ed4f15bf0fb
s4ndhyac <sandhya2392@gmail.com>
2019-11-06 03:49:53 -0800

    Update GoogleLoginButton.js

--- source/frontend/src/components/oauth/GoogleLoginButton.js
+++ source/frontend/src/components/oauth/GoogleLoginButton.js
@@ -1,13 +1,25 @@
 import React from "react";
 import GoogleLogin from "react-google-login";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import { connect } from "react-redux";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 import { handleLoginSuccess, clientId, cookiePolicy } from "./index";
 import { setUser } from "../../actions";
 
 class GoogleLoginButton extends React.Component {
   render() {
+    const { currentUser } = this.props;
+    if (!!currentUser) {
+      return (
+        <Button variant="contained" color="inherit">
+          <Typography variant="h6">
+            <Link to="/dashboard" style={{ color: '#000', textDecoration: 'none' }}>Go To Dashboard</Link>
+          </Typography>
+        </Button>
+      );
+    }
     return (
       <GoogleLogin
         clientId={clientId}
@@ -20,8 +32,14 @@ class GoogleLoginButton extends React.Component {
   }
 }
 
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+
 const connectedComponent = connect(
-  null,
+  mapStateToProps,
   { setUser }
 )(GoogleLoginButton);
 
