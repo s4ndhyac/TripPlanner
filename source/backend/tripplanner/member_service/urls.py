d7cfc9ae53b27f773652c7e2b89835b778ae3905
s4ndhyac <sandhya2392@gmail.com>
2019-11-09 16:58:35 -0800

    fix urls

--- source/backend/tripplanner/member_service/urls.py
+++ source/backend/tripplanner/member_service/urls.py
@@ -4,12 +4,13 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'', views.GroupViewSet)
+router.register(r'group', views.GroupViewSet, 'group')
+router.register(r'user', views.UserViewSet, 'user')
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('logout/', views.logout, name='logout'),
     path('auth-user/', views.authenticate, name='authenticate'),
     path('addGroup/', views.addGroup, name='addGroup'),
-    url(r'^group/', include(router.urls))
+    url(r'^v1/', include(router.urls))
 ]
