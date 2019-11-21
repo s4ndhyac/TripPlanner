2207fbe6312cc99c0c766bb73299b216a11ccf0a
latonn <wannafly0110@gmail.com>
2019-11-20 17:31:03 -0800

    Modified TripPlanner email authentification

--- source/backend/tripplanner/tripplanner/settings.py
+++ source/backend/tripplanner/tripplanner/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tripplanner',
-        'USER': '',
+        'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
